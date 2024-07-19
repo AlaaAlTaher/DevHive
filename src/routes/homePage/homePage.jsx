c498678651c316c3f64b9416175dba0b957aa3e8
Alaa-Al-Taher <147681763+Alaa-Al-Taher@users.noreply.github.com>
2024-07-19 20:16:52 +0300

    migrated code

---
+++ src/routes/homePage/homePage.jsx
@@ -0,0 +1,41 @@
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+
+function HomePage() {
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">From Developers, To Developers</h1>
+          <p>
+            DevHive is Jordan's premier freelancing platform for developers. We
+            connect talented developers with exciting projects, providing a
+            space to showcase skills, collaborate, and grow. Join us today to
+            advance your freelancing career and be a part of Jordan's thriving
+            tech community.
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Awards Gained</h2>
+            </div>
+            <div className="box">
+              <h1>1200+</h1>
+              <h2>Job Opportunities</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
